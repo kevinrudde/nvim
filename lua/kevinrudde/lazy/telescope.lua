51e878064e44d2c4740b15af06874c5d0819f5d7
Kevin Rudde <k.rudde@shopware.com>
2024-07-18 11:01:50 +0200

    Update find_files

--- lua/kevinrudde/lazy/telescope.lua
+++ lua/kevinrudde/lazy/telescope.lua
@@ -9,9 +9,11 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({ hidden = true })
+        end)
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        
+
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
