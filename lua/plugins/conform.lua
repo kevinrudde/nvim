edf6ea92b3c460d44b80dbd7cfa74283dddebd55
Kevin Rudde <k.rudde@shopware.com>
2025-12-12 10:42:10 +0100

    feat: add conform, go and opencode enhancements

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      yaml = { "yamlfmt" },
+    },
+    formatters = {
+      yamlfmt = {
+        command = "yamlfmt",
+        args = { "-formatter", "basic", "-indentless_arrays=true" },
+      },
+    },
+  },
+}
