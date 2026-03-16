78e47617c1a6d5647447a2e913d6d3c58a04da5e
Kevin Rudde <k.rudde@shopware.com>
2026-03-16 09:17:38 +0100

    chore: switch lazygit shortcuts

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Toggle Snacks explorer with <leader>e
+local map = vim.keymap.set
+
+if vim.fn.executable("lazygit") == 1 then
+  map("n", "<leader>gG", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" })
+  map("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
+end
