6c7fba1c1ad9bbd8b2ca4449cc3a5c3be2d49b3c
Kevin Rudde <k.rudde@shopware.com>
2026-04-07 09:12:03 +0200

    feat: override paste keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,7 @@ if vim.fn.executable("lazygit") == 1 then
   map("n", "<leader>gG", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" })
   map("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
 end
+
+map("x", "p", [["_dP]], { desc = "Paste Without Replacing Clipboard" })
+map("x", "<C-v>", [["_dP]], { desc = "Paste Without Replacing Clipboard" })
+map("x", "<D-v>", [["_dP]], { desc = "Paste Without Replacing Clipboard" })
