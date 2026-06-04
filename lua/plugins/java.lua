35e5de0b9ccd6016f9a6b53948b602d5492279a4
Kevin Rudde <k.rudde@shopware.com>
2026-06-04 13:13:22 +0200

    feat: improve java setup

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -6,6 +6,19 @@ return {
       opts.jdtls.handlers = vim.tbl_extend("force", opts.jdtls.handlers or {}, {
         ["$/progress"] = function() end,
       })
+      -- prevent .settings, .project, etc files from being generated in the project folder
+      table.insert(opts.cmd, "--jvm-arg=-Djava.import.generatesMetadataFilesAtProjectRoot=false")
+      table.insert(opts.cmd, "-Xmx8G")
+
+      opts.settings = {
+        java = {
+          format = {
+            enabled = true,
+            comments = { enabled = false },
+            tabSize = 2,
+          },
+        },
+      }
     end,
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       ensure_installed = {
         "harper-ls",
+        "jdtls",
       },
     },
     keys = {
@@ -28,6 +29,9 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = {
+         enabled = false,
+      },
       servers = {
         harper_ls = {
           autostart = false,
